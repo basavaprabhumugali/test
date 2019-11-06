e240d0560b8285988f24a11ec8edffecbca423f9
EE211089 <ee211089@pcz-ee211089.sasken.com>
2019-11-06 10:21:57 +0530

    changes i have done in p1.c

---
+++ p1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<sys/types.h>
+//#include<unistd.h>
+
+
+int hello(void)
+{
+int x=1;
+int y=x+1;
+return printf("%d\n",x+y);
+}
+
+
+
+
+
+int main()
+{
+int d,a=1,b=2,c=3;
+d=a^2+b^2+c^2;
+	hello();
+	printf("pid=%d , ppid=%d\ni, d=%d\n",getpid(),getppid(),d);
+}
